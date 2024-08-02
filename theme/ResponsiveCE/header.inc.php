2c87f82a95d35b7eeeb2e6313896054e8fbf8473
risingisland <risingisland@yahoo.com>
2024-08-02 11:39:22 +0100

    ResponsiveCE theme

--- theme/ResponsiveCE/header.inc.php
+++ theme/ResponsiveCE/header.inc.php
@@ -19,9 +19,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php get_page_clean_title(); ?> | <?php get_site_name(); ?></title>
     <link rel="stylesheet" type="text/css" href="<?php get_theme_url(); ?>/css/pico.min.css">
-    <?php echo ($bootstrap !== 'yes' ?  '' : '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap-grid.min.css" integrity="sha512-JQksK36WdRekVrvdxNyV3B0Q1huqbTkIQNbz1dlcFVgNynEMRl0F8OSqOGdVppLUDIvsOejhr/W5L3G/b3J+8w==" crossorigin="anonymous" referrerpolicy="no-referrer" />'); ?>
+    <?php echo ($bootstrap !== 'yes' ?  '' : '<link rel="stylesheet" href="'.$SITEURL.'theme/ResponsiveCE/css/bootstrap-grid.min.css">'); ?>
     <link rel="stylesheet" type="text/css" href="<?php get_theme_url(); ?>/css/style.css?v=<?php echo rand(); ?>">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php get_theme_url(); ?>/css/all.min.css">
     <?php get_header(); ?>
 </head>
 
