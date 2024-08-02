526739f46d830c8b20b6d5e7014ffdac20e6d3d2
risingisland <risingisland@yahoo.com>
2024-08-02 11:33:37 +0100

    Upgrade CE langs

--- plugins/UpgradeCE/lang/ru_RU.php
+++ plugins/UpgradeCE/lang/ru_RU.php
@@ -3,37 +3,37 @@
 $i18n = [
 	
 # Basics
-	'lang_Menu_Title'			=>	'Upgrade CE',
+	'lang_Menu_Title'			=>	'Обновление CE',
 	
-	'lang_Page_Title'			=>	'Upgrade CE',
-	'lang_Description'			=>	'Upgrade your install to the latest version.',
+	'lang_Page_Title'			=>	'Обновление CE',
+	'lang_Description'			=>	'Обновите свою установку до последней версии.',
 	
 # General
-	'lang_Installed_Version'	=>	'Installed Version',
-	'lang_Note'					=>	'Note',
-	'lang_Requirement'			=>	'Minimum requirement of v3.3.16.',
-	'lang_Create_Backup'		=>	'Always create a backup to protect against the unexpected!',
-	'lang_Themes_Overwritten'	=>	'Themes <u>ResponsiveCE</u> & <u>W3-Startup</u> will be overwritten.',
+	'lang_Installed_Version'	=>	'Установленная версия',
+	'lang_Note'					=>	'Примечание',
+	'lang_Requirement'			=>	'Требуется версия не менее 3.3.16',
+	'lang_Create_Backup'		=>	'Всегда создавайте резервную копию для защиты от непредвиденных обстоятельств!',
+	'lang_Themes_Overwritten'	=>	'Темы <u>ResponsiveCE</u> и <u>W3-Startup</u> будут перезаписаны.',
 	
-	'lang_Key_Changes'			=>	'Key changes in this update',
-	'lang_New'					=>	'New',
-	'lang_Updated'				=>	'Updated',
-	'lang_Fixes'				=>	'Fixes',
-	'lang_Security'				=>	'Security',
-	'lang_More_Info'			=>	'More info: Github Repo',
-	'lang_Download'				=>	'Direct Download Only',
-	'lang_Update_Now'			=>	'Update Now',
+	'lang_Key_Changes'			=>	'Основные изменения в этом обновлении',
+	'lang_New'					=>	'Новое',
+	'lang_Updated'				=>	'Обновлено',
+	'lang_Fixes'				=>	'Исправления',
+	'lang_Security'				=>	'Безопасность',
+	'lang_More_Info'			=>	'Дополнительная информация: Репозиторий на Github',
+	'lang_Download'				=>	'Только прямая загрузка',
+	'lang_Update_Now'			=>	'Обновите сейчас',
 	
-	'lang_New_as_of'			=>	'New as of',
-	'lang_Plugin_MA'			=>	'Plugin "Massive Admin Theme" is included and may need to be activated.',
-	'lang_Update_gsConfig'		=>	'If you are updating from a version prior to this, you will need to manually update your existing "gsconfig.php" with the following...',
-	'lang_Add_New'				=>	'Add New',
-	'lang_Replace_section'		=>	'Replace section',
-	'lang_With_updated'			=>	'With updated',
+	'lang_New_as_of'			=>	'Новая версия на',
+	'lang_Plugin_MA'			=>	'Плагин "Massive Admin Theme" включен в комплект поставки, и, возможно, его потребуется активировать.',
+	'lang_Update_gsConfig'		=>	'Если вы обновляетесь с более ранней версии, вам потребуется вручную обновить существующий "gsconfig.php" следующим образом...',
+	'lang_Add_New'				=>	'Добавить новый',
+	'lang_Replace_section'		=>	'Заменить раздел',
+	'lang_With_updated'			=>	'С обновленнием',
 	
-	'lang_Installing'			=>	'Installing...',
-	'lang_Seconds_remaining'	=>	'seconds remaining',
-	'lang_Finished'				=>	'Finished... Redirecting...',
+	'lang_Installing'			=>	'Установка...',
+	'lang_Seconds_remaining'	=>	'оставшиеся секунды',
+	'lang_Finished'				=>	'Закончено... Перенаправление...',
 	
 	'lang_Icon'					=>	'<svg xmlns="http://www.w3.org/2000/svg" style="vertical-align:middle;" width="2em" height="2em" viewBox="0 0 24 24"><path fill="#CF3805" d="M4 16h4v4H4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".2" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 16h4v4h-4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".4" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 16h4v4h-4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".6" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M4 10h4v4H4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".8" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 10h4v4h-4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 10h4v4h-4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.2" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M4 4h4v4H4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.4" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 4h4v4h-4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.6" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 4h4v4h-4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.8" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path></svg> ',
 	
