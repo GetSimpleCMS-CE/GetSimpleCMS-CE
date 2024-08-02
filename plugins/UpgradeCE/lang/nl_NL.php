25da81a79390d8877eea16b05ac29433e4e604ea
risingisland <risingisland@yahoo.com>
2024-08-02 11:34:14 +0100

    Upgrade CE langs

--- plugins/UpgradeCE/lang/nl_NL.php
+++ plugins/UpgradeCE/lang/nl_NL.php
@@ -3,37 +3,37 @@
 $i18n = [
 	
 # Basics
-	'lang_Menu_Title'			=>	'Upgrade CE',
+	'lang_Menu_Title'			=>	'Upgraden CE',
 	
-	'lang_Page_Title'			=>	'Upgrade CE',
-	'lang_Description'			=>	'Upgrade your install to the latest version.',
+	'lang_Page_Title'			=>	'Upgraden CE',
+	'lang_Description'			=>	'Upgraden uw installatie bij naar de nieuwste versie.',
 	
 # General
-	'lang_Installed_Version'	=>	'Installed Version',
-	'lang_Note'					=>	'Note',
-	'lang_Requirement'			=>	'Minimum requirement of v3.3.16.',
-	'lang_Create_Backup'		=>	'Always create a backup to protect against the unexpected!',
-	'lang_Themes_Overwritten'	=>	'Themes <u>ResponsiveCE</u> & <u>W3-Startup</u> will be overwritten.',
+	'lang_Installed_Version'	=>	'Geïnstalleerde Versie',
+	'lang_Note'					=>	'Opmerking',
+	'lang_Requirement'			=>	'Minimale vereiste van v3.3.16.',
+	'lang_Create_Backup'		=>	'Maak altijd een back-up om jezelf te beschermen tegen het onverwachte!',
+	'lang_Themes_Overwritten'	=>	'Thematische ResponsiveCE & W3-Startup zullen worden overschreven.',
 	
-	'lang_Key_Changes'			=>	'Key changes in this update',
-	'lang_New'					=>	'New',
-	'lang_Updated'				=>	'Updated',
-	'lang_Fixes'				=>	'Fixes',
-	'lang_Security'				=>	'Security',
-	'lang_More_Info'			=>	'More info: Github Repo',
-	'lang_Download'				=>	'Direct Download Only',
-	'lang_Update_Now'			=>	'Update Now',
+	'lang_Key_Changes'			=>	'Belangrijke veranderingen in deze update',
+	'lang_New'					=>	'Nieuw',
+	'lang_Updated'				=>	'Bijgewerkt',
+	'lang_Fixes'				=>	'Reparaties',
+	'lang_Security'				=>	'Veiligheid',
+	'lang_More_Info'			=>	'Meer informatie: Github-archief',
+	'lang_Download'				=>	'Alleen directe download',
+	'lang_Update_Now'			=>	'Bijwerken nu',
 	
-	'lang_New_as_of'			=>	'New as of',
-	'lang_Plugin_MA'			=>	'Plugin "Massive Admin Theme" is included and may need to be activated.',
-	'lang_Update_gsConfig'		=>	'If you are updating from a version prior to this, you will need to manually update your existing "gsconfig.php" with the following...',
-	'lang_Add_New'				=>	'Add New',
-	'lang_Replace_section'		=>	'Replace section',
-	'lang_With_updated'			=>	'With updated',
+	'lang_New_as_of'			=>	'Nieuw vanaf',
+	'lang_Plugin_MA'			=>	'De plugin "Massive Admin Theme" is inbegrepen en moet mogelijk worden geactiveerd.',
+	'lang_Update_gsConfig'		=>	'Als u bijwerkt vanaf een versie voor deze, moet u handmatig uw bestaande "gsconfig.php" bijwerken met het volgende...',
+	'lang_Add_New'				=>	'Voeg Nieuwe Toe',
+	'lang_Replace_section'		=>	'Vervang sectie',
+	'lang_With_updated'			=>	'Met bijgewerkte',
 	
-	'lang_Installing'			=>	'Installing...',
-	'lang_Seconds_remaining'	=>	'seconds remaining',
-	'lang_Finished'				=>	'Finished... Redirecting...',
+	'lang_Installing'			=>	'Installeren...',
+	'lang_Seconds_remaining'	=>	'seconden overblijven',
+	'lang_Finished'				=>	'Klaar...Het doorverwijzen...',
 	
 	'lang_Icon'					=>	'<svg xmlns="http://www.w3.org/2000/svg" style="vertical-align:middle;" width="2em" height="2em" viewBox="0 0 24 24"><path fill="#CF3805" d="M4 16h4v4H4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".2" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 16h4v4h-4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".4" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 16h4v4h-4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".6" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M4 10h4v4H4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".8" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 10h4v4h-4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 10h4v4h-4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.2" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M4 4h4v4H4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.4" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 4h4v4h-4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.6" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 4h4v4h-4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.8" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path></svg> ',
 	
