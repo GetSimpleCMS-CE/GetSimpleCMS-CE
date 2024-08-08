2c974e02a77ece6130449ccc00973567beec1d23
risingisland <risingisland@yahoo.com>
2024-08-08 10:09:23 +0100

    Update temp.gsconfig.php

--- temp.gsconfig.php
+++ temp.gsconfig.php
@@ -22,28 +22,28 @@ if (basename($_SERVER['PHP_SELF']) == 'gsconfig.php') {
 # define('GSUSECUSTOMSALT', 'your_new_salt_value_here');
 
 # Default thumbnail width of uploaded image
-define('GSIMAGEWIDTH', '480');
+define('GSIMAGEWIDTH','480');
 
 # Change the administrative panel folder name
-# define('GSADMIN', 'admin');
+# define('GSADMIN','admin');
 
 # Turn on debug mode
-# define('GSDEBUG', TRUE);
+# define('GSDEBUG',TRUE);
 
 # Ping search engines upon sitemap generation?
-define('GSDONOTPING', 1);
+define('GSDONOTPING',1);
 
 # Turn off CSRF protection. Uncomment this if you keep receiving the error message "CSRF error detected..."
-# define('GSNOCSRF', TRUE);
+# define('GSNOCSRF',TRUE);
 
 # Set override CHMOD mode
-# define('GSCHMOD', 0755);
+# define('GSCHMOD',0755);
 
 # Disable chmod operations
-# define('GSDOCHMOD',false);
+# define('GSDOCHMOD',FALSE);
 
 # Enable Canonical Redirects?
-# define('GSCANONICAL', 1);
+# define('GSCANONICAL',1);
 
 # Login Page Default Language: en_EN, es_ES, pl_PL, de_DE, uk_UK, etc.
 $LANG = 'en_EN';
@@ -52,80 +52,79 @@ $LANG = 'en_EN';
 define('GSSORTPAGELISTBY','menu');
 
 # WYSIWYG editor height (default 500)
-# define('GSEDITORHEIGHT', '400');
+# define('GSEDITORHEIGHT','400');
 
 # WYSIWYG toolbars (advanced, basic, CEbar, island or [custom config])
-define('GSEDITORTOOL', "CEbar");
+define('GSEDITORTOOL','CEbar');
 
 # WYSIWYG editor language (default en)
-# define('GSEDITORLANG', 'en');
+# define('GSEDITORLANG','en');
 
 # WYSIWYG Editor Options
 define('GSEDITOROPTIONS', '
 extraPlugins:"fontawesome5,youtube,codemirror,cmsgrid,colorbutton,oembed,simplebutton,spacingsliders",
-disableNativeSpellChecker : false,
-forcePasteAsPlainText : true
+disableNativeSpellChecker : FALSE,
+forcePasteAsPlainText : TRUE
 ');
 
 # Set email from address
-# define('GSFROMEMAIL', 'noreply@get-simple.info');
+# define('GSFROMEMAIL','noreply@get-simple.info');
 
 # Autosave within edit.php. Value is the autosave interval in seconds
-# define('GSAUTOSAVE', 900);
+# define('GSAUTOSAVE',900);
 
 # Enable the External API to be shown on settings page 
-# define('GSEXTAPI', 1);
+# define('GSEXTAPI',1);
 	
 # Set PHP locale
 # http://php.net/manual/en/function.setlocale.php
-# setlocale(LC_ALL, 'en_US');
+# setlocale(LC_ALL,'en_US');
 
-# Define default timezone of server, accepts php timezone string
+# Set default timezone of server, accepts php timezone string
 # valid timeszones can be found here http://www.php.net/manual/en/timezones.php
-# define('GSTIMEZONE', 'America/Chicago');
-# define('GSTIMEZONE', 'Europe/London');
+# Europe/London, Australia/Sydney, Etc/GMT-8
+# define('GSTIMEZONE','America/Chicago');
 
 # Disable loading of external CDN versions of scripts (jQuery/jQueryUI)
-define("GSNOCDN",true);
+define("GSNOCDN",TRUE);
 
 # Disable Codemirror theme editor
-# define("GSNOHIGHLIGHT",true);
+# define("GSNOHIGHLIGHT",TRUE);
 
-# Forces suppression of php errors when GSDEBUG is false, despite php ini settings
-define('GSSUPPRESSERRORS',true);
+# Forces suppression of php errors when GSDEBUG is FALSE, despite php ini settings
+define('GSSUPPRESSERRORS',TRUE);
 
-# Disable check for Apache web server, default false
-#define('GSNOAPACHECHECK', true);
+# Disable check for Apache web server, default FALSE
+#define('GSNOAPACHECHECK',TRUE);
 
 # Disable header version check
-#define('GSNOVERCHECK', true);
+#define('GSNOVERCHECK',TRUE);
 
 # Disable Sitemap generation and menu items
-# define('GSNOSITEMAP',true);
+# define('GSNOSITEMAP',TRUE);
 
 # Enable auto meta descriptions from content excerpts when empty
-define('GSAUTOMETAD',true);
+define('GSAUTOMETAD',TRUE);
 
 # Set default language for missing lang token merge, 
-# accepts a lang string, default is 'en_US', false to disable
-# define('GSMERGELANG',false);
+# accepts a lang string, default is 'en_US', FALSE to disable
+# define('GSMERGELANG',FALSE);
 
 # GS can prevent backend or frontend pages from being loaded inside a frame 
 # this is done by sending an x-frame-options header, and helps protect against clickjacking attacks
-# This is enabled by default for backend pages (true/GSBACK)
-# setting GSNOFRAME to (false) will disable this behavior
+# This is enabled by default for backend pages (TRUE/GSBACK)
+# setting GSNOFRAME to (FALSE) will disable this behavior
 # You can also customize this by passing the gs location definitions,
-# GSFRONT, GSBACK or GSBOTH definitions enable this for front and/or backends
+# GSFRONT, GSBACK, GSBOTH or FALSE for never definitions enable this for front and/or backends
 # define('GSNOFRAME',GSBOTH); # prevent in frames ALWAYS
-# define('GSNOFRAME',false);  # prevent in frames NEVER
 
 # GS can format its xml files before saving them if you require human readable source for them
-# define('GSFORMATXML',true);
+# define('GSFORMATXML',TRUE);
 
 # Hide I18N text on View All Pages.
-# define('I18N_SINGLE_LANGUAGE', true);
+# define('I18N_SINGLE_LANGUAGE',TRUE);
 
 # Ignore user browser language
-# define('I18N_IGNORE_USER_LANGUAGE',true);
+# define('I18N_IGNORE_USER_LANGUAGE',TRUE);
 
 ?>
