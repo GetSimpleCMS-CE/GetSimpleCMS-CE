249bc78f83379f50d170c55827a5ab2e6fe4dc98
risingisland <risingisland@yahoo.com>
2025-02-26 14:05:49 +0000

    Add files via upload

---
+++ plugins/UpdateCE/lang/ar_SA.php
@@ -0,0 +1,41 @@
+<?php
+// تمت الترجمة بواسطة: fahad4x4
+
+$i18n = [
+	
+# الأساسيات
+	'lang_Menu_Title'			=>	'تحديث CE',
+	
+	'lang_Page_Title'			=>	'تحديث CE',
+	'lang_Description'			=>	'قم بتحديث تثبيتك إلى أحدث إصدار.',
+	
+# عام
+	'lang_Installed_Version'	=>	'الإصدار المثبت',
+	'lang_Note'					=>	'ملاحظة',
+	'lang_Requirement'			=>	'الحد الأدنى المطلوب هو الإصدار v3.3.16.',
+	'lang_Create_Backup'		=>	'قم دائمًا بإنشاء نسخة احتياطية لحماية نفسك من المفاجآت!',
+	'lang_Themes_Overwritten'	=>	'سيتم استبدال السمات <u>ResponsiveCE</u> و <u>W3-Startup</u>.',
+	
+	'lang_Key_Changes'			=>	'التغييرات الرئيسية في هذا التحديث',
+	'lang_New'					=>	'جديد',
+	'lang_Updated'				=>	'تم التحديث',
+	'lang_Fixes'				=>	'إصلاحات',
+	'lang_Security'				=>	'الأمان',
+	'lang_More_Info'			=>	'مزيد من المعلومات: مستودع Github',
+	'lang_Download'				=>	'تنزيل مباشر فقط',
+	'lang_Update_Now'			=>	'تحديث الآن',
+	
+	'lang_New_as_of'			=>	'جديد اعتبارًا من',
+	'lang_Plugin_MA'			=>	'الملحق "Massive Admin Theme" متضمن وقد يحتاج إلى التفعيل.',
+	'lang_Update_gsConfig'		=>	'إذا كنت تقوم بالتحديث من إصدار أقدم، فستحتاج إلى تحديث ملف "gsconfig.php" يدويًا بإضافة ما يلي...',
+	'lang_Add_New'				=>	'إضافة جديد',
+	'lang_Replace_section'		=>	'استبدال القسم',
+	'lang_With_updated'			=>	'بالإصدار المحدث',
+	
+	'lang_Installing'			=>	'جاري التثبيت...',
+	'lang_Seconds_remaining'	=>	'ثوانٍ متبقية',
+	'lang_Finished'				=>	'تم الانتهاء... جاري التوجيه...',
+	
+	'lang_Icon'					=>	'<svg xmlns="http://www.w3.org/2000/svg" style="vertical-align:middle;" width="2em" height="2em" viewBox="0 0 24 24"><path fill="#CF3805" d="M4 16h4v4H4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".2" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 16h4v4h-4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".4" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 16h4v4h-4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".6" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M4 10h4v4H4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".8" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 10h4v4h-4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 10h4v4h-4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.2" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M4 4h4v4H4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.4" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 4h4v4h-4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.6" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 4h4v4h-4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.8" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path></svg> ',
+	
+];
