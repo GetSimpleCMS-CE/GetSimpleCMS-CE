ef514522af90b1d3657ba67ef3e43ad3ae2e78b0
risingisland <risingisland@yahoo.com>
2025-05-07 09:56:04 +0100

    Add files via upload

---
+++ plugins/UpdateCE/lang/nb_NO.php
@@ -0,0 +1,43 @@
+<?php
+
+$i18n = [
+
+# Basics
+'lang_Menu_Title'	=>	'Oppdater CE',
+'lang_Page_Title'	=>	'Oppdater CE',
+'lang_Description'	=>	'Oppdater din installasjon til den nyeste versjonen.',
+
+# General
+'lang_Installed_Version'	=>	'Installert Versjon',
+'lang_Note'	=>	'Merk',
+'lang_Requirement'	=>	'Minimumskrav er v3.3.16.',
+'lang_Create_Backup'	=>	'Lag alltid en <b><a class="w3-text-light w3-orange w3-round w3-padding" href="archive.php">Sikkerhetskopi</a></b> for å beskytte deg mot det uventede!',
+'lang_Themes_Overwritten'	=>	'Temaene <u>ResponsiveCE</u> & <u>W3-Startup</u> vil bli overskrevet.',
+'lang_Rename_Admin'	=>	'Hvis du har endret navn på standard <b>/admin/</b>-mappen, må dette <u>tilbakestilles før</u> du utfører denne oppdateringen. <br>Etter at du har utført oppdateringen, kan du personalisere den igjen.',
+
+'lang_Key_Changes'	=>	'Viktige endringer i denne oppdateringen',
+'lang_New'	=>	'Ny',
+'lang_Updated'	=>	'Oppdatert',
+'lang_Fixes'	=>	'Feilrettinger',
+'lang_Security'	=>	'Sikkerhet',
+'lang_More_Info'	=>	'Mer info: Github Repo',
+'lang_Wiki'	=>	'Wiki',
+'lang_Download'	=>	'Kun Direkte Nedlasting',
+'lang_Update_Now'	=>	'Oppdater Nå',
+
+'lang_New_as_of'	=>	'Ny fra og med',
+'lang_Plugin_MA'	=>	'Plugin-en "Massive Admin Theme" er inkludert og kan trenge aktivering.',
+'lang_Update_gsConfig'	=>	'Hvis du oppdaterer fra en tidligere versjon enn denne, må du manuelt oppdatere din eksisterende "<b>gsconfig.php</b>" med følgende...',
+'lang_Add_New'	=>	'Legg til ny',
+'lang_Replace_section'	=>	'Erstatt seksjon',
+'lang_With_updated'	=>	'Med oppdatert',
+
+'lang_Installing'	=>	'Installerer...',
+'lang_Seconds_remaining'	=>	'sekunder igjen',
+'lang_Finished'	=>	'Ferdig... Videresender...',
+
+'lang_No_Updates'	=>	'Ingen oppdateringer er nødvendig. Du bruker den nyeste versjonen.',
+
+'lang_Icon'	=>	'<svg xmlns="http://www.w3.org/2000/svg" style="vertical-align:middle;" width="2em" height="2em" viewBox="0 0 24 24"><path fill="#CF3805" d="M4 16h4v4H4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".2" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 16h4v4h-4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".4" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 16h4v4h-4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".6" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M4 10h4v4H4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".8" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 10h4v4h-4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 10h4v4h-4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.2" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M4 4h4v4H4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.4" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 4h4v4h-4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.6" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 4h4v4h-4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.8" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path></svg> ',
+
+];
---
+++ plugins/UpdateCE/lang/sv_SE.php
@@ -0,0 +1,43 @@
+<?php
+
+$i18n = [
+
+# Basics
+'lang_Menu_Title'	=>	'Uppdatera CE',
+'lang_Page_Title'	=>	'Uppdatera CE',
+'lang_Description'	=>	'Uppdatera din installation till den senaste versionen.',
+
+# General
+'lang_Installed_Version'	=>	'Installerad Version',
+'lang_Note'	=>	'Notera',
+'lang_Requirement'	=>	'Minimumkrav är v3.3.16.',
+'lang_Create_Backup'	=>	'Skapa alltid en <b><a class="w3-text-light w3-orange w3-round w3-padding" href="archive.php">Säkerhetskopia</a></b> för att skydda dig mot det oväntade!',
+'lang_Themes_Overwritten'	=>	'Temana <u>ResponsiveCE</u> & <u>W3-Startup</u> kommer att skrivas över.',
+'lang_Rename_Admin'	=>	'Om du har bytt namn på standardmappen <b>/admin/</b> måste detta <u>återställas innan</u> du utför denna uppdatering. <br>Efter att du har utfört uppdateringen kan du anpassa den igen.',
+
+'lang_Key_Changes'	=>	'Viktiga ändringar i denna uppdatering',
+'lang_New'	=>	'Ny',
+'lang_Updated'	=>	'Uppdaterad',
+'lang_Fixes'	=>	'Bugfixar',
+'lang_Security'	=>	'Säkerhet',
+'lang_More_Info'	=>	'Mer info: Github Repo',
+'lang_Wiki'	=>	'Wiki',
+'lang_Download'	=>	'Endast Direkt Nedladdning',
+'lang_Update_Now'	=>	'Uppdatera Nu',
+
+'lang_New_as_of'	=>	'Nytt från och med',
+'lang_Plugin_MA'	=>	'Pluginet "Massive Admin Theme" är inkluderat och kan behöva aktiveras.',
+'lang_Update_gsConfig'	=>	'Om du uppdaterar från en tidigare version än denna måste du manuellt uppdatera din befintliga "<b>gsconfig.php</b>" med följande...',
+'lang_Add_New'	=>	'Lägg till ny',
+'lang_Replace_section'	=>	'Ersätt sektion',
+'lang_With_updated'	=>	'Med uppdaterad',
+
+'lang_Installing'	=>	'Installerar...',
+'lang_Seconds_remaining'	=>	'sekunder kvar',
+'lang_Finished'	=>	'Klar... Omdirigerar...',
+
+'lang_No_Updates'	=>	'Inga uppdateringar behövs. Du använder den senaste versionen.',
+
+'lang_Icon'	=>	'<svg xmlns="http://www.w3.org/2000/svg" style="vertical-align:middle;" width="2em" height="2em" viewBox="0 0 24 24"><path fill="#CF3805" d="M4 16h4v4H4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".2" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 16h4v4h-4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".4" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 16h4v4h-4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".6" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M4 10h4v4H4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".8" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 10h4v4h-4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 10h4v4h-4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.2" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M4 4h4v4H4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.4" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 4h4v4h-4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.6" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 4h4v4h-4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.8" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path></svg> ',
+
+];
