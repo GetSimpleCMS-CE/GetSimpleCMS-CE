4c63d7a89db60f5295104967617f2ccf75c45840
risingisland <risingisland@yahoo.com>
2025-06-15 09:11:44 +0100

    Create ja_JP.php

---
+++ plugins/UpdateCE/lang/ja_JP.php
@@ -0,0 +1,44 @@
+<?php
+
+$i18n = [
+	
+# Basics
+	'lang_Menu_Title'			=>	'CE の更新',
+	
+	'lang_Page_Title'			=>	'CE の更新',
+	'lang_Description'			=>	'インストールを最新バージョンに更新してください。',
+	
+# General
+	'lang_Installed_Version'	=>	'Installed Version',
+	'lang_Note'					=>	'Note',
+	'lang_Requirement'			=>	'Minimum requirement of v3.3.16.',
+	'lang_Create_Backup'		=>	'Always create a <b><a class="w3-text-light w3-orange w3-round w3-padding" href="archive.php">Backup</a></b> to protect against the unexpected!',
+	'lang_Themes_Overwritten'	=>	'Themes <u>ResponsiveCE</u> & <u>W3-Startup</u> will be overwritten.',
+	'lang_Rename_Admin'			=>	'If you have renamed the default <b>/admin/</b> folder, this needs to be <u>reverted back before</u> applying this update. <br>After you have applied the update, you may again personalize this.',
+	
+	'lang_Key_Changes'			=>	'Key changes in this update',
+	'lang_New'					=>	'New',
+	'lang_Updated'				=>	'Updated',
+	'lang_Fixes'				=>	'Fixes',
+	'lang_Security'				=>	'Security',
+	'lang_More_Info'			=>	'More info: Github Repo',
+	'lang_Wiki'					=>	'Wiki',
+	'lang_Download'				=>	'Direct Download Only',
+	'lang_Update_Now'			=>	'Update Now',
+	
+	'lang_New_as_of'			=>	'New as of',
+	'lang_Plugin_MA'			=>	'Plugin "Massive Admin Theme" is included and may need to be activated.',
+	'lang_Update_gsConfig'		=>	'If you are updating from a version prior to this, you will need to manually update your existing "<b>gsconfig.php</b>" with the following...',
+	'lang_Add_New'				=>	'Add New',
+	'lang_Replace_section'		=>	'Replace section',
+	'lang_With_updated'			=>	'With updated',
+	
+	'lang_Installing'			=>	'Installing...',
+	'lang_Seconds_remaining'	=>	'seconds remaining',
+	'lang_Finished'				=>	'Finished... Redirecting...',
+	
+	'lang_No_Updates'				=>	'No updates are needed. You are using the current version.',
+	
+	'lang_Icon'					=>	'<svg xmlns="http://www.w3.org/2000/svg" style="vertical-align:middle;" width="2em" height="2em" viewBox="0 0 24 24"><path fill="#CF3805" d="M4 16h4v4H4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".2" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 16h4v4h-4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".4" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 16h4v4h-4V16z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".6" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M4 10h4v4H4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin=".8" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 10h4v4h-4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 10h4v4h-4V10z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.2" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M4 4h4v4H4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.4" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M10 4h4v4h-4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.6" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path><path fill="#CF3805" d="M16 4h4v4h-4V4z" class="st0"><animate fill="remove" accumulate="none" additive="replace" attributeName="opacity" begin="1.8" calcMode="linear" dur="3s" keyTimes="0;0.9;1" repeatCount="indefinite" restart="always" values="1;0;0"/></path></svg> ',
+	
+];
