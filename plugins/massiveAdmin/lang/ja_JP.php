637e524514d9a0812e3431f57f8ca7564ade48ae
risingisland <risingisland@yahoo.com>
2025-06-15 09:11:41 +0100

    Create ja_JP.php

---
+++ plugins/massiveAdmin/lang/ja_JP.php
@@ -0,0 +1,169 @@
+<?php
+
+$i18n = [
+	'MAITENANCETITLE' 				=> 'メンテナンス モード オプション',
+	'MAINTENANCE_ON' 				=> 'メンテナンス モード オン?',
+	'CONTENT_MAINTENANCE_MODE' 		=> 'メンテナンス モードの内容:',
+	'TURNONBOOTSTRAPGRID' 			=> 'Bootstrap-Grid 4.0 をオンにしますか?',
+	'TURNONBOOTSTRAPGRIDONTHEME' 	=> 'Bootstrap-Grid CSS をフロントエンド テーマに追加しますか?',
+	'YES' 							=> 'はい'、
+	'NO' 							=> 'いいえ'、
+	'SAVEOPTION' 					=> '変更を保存',
+	'SUPPORT' 						=> 'PayPal 経由で私の仕事をサポートしたい場合:) ありがとう! '、
+	'UPLOADFILE' 					=> 'ファイルをアップロード',
+	'ANOTHERPAGE' 					=> '<strong>変更が保存されました!</strong> 別のページに移動します:)',
+	'EDITCOMPONENTS' 				=> 'コンポーネントの編集',
+	'MASSIVEADMINSETTINGSTITLE' 	=> 'MassiveAdmin 設定',
+	'DROPFILES' 					=> 'ファイルをここにドロップしてアップロード',
+	'BOOTSTRAPTITLE' 				=> 'ブートストラップ グリッド オプション',
+
+	//options on file edit
+	'COPYFILE' 						=> 'ファイルをコピー',
+	'RENAMEFILE' 					=> '名前変更/ディレクトリ変更',
+	'DOWNLOADFILE' 					=> 'ダウンロード',
+	'INFOCOPY' 						=> 'コピーされたファイルは ',
+	'INFOERROR' 					=> 'この名前のファイルはこのディレクトリにすでに存在します ',
+	'FILENOW' 						=> 'ファイルは次の場所にあります',
+
+	//options on Create New User
+	'CREATENEWUSER' 				=> '新しいユーザーの作成',
+	'USERNAMECREATE' 				=> 'ユーザー名:',
+	'PASSWORDCREATE' 				=> 'パスワード:',
+	'EMAILCREATE' 					=> '新しいユーザーの電子メール:',
+	'LANGCREATE' 					=> 'ユーザー言語:',
+
+	//options hidden section
+	'HIDEMENUTITLE' 				=> 'ユーザーマネージャーとセクション',
+	'SHOW' 							=> '表示'、
+	'HIDE' 							=> '隠す'、
+	'HIDSECTIONTITLE' 				=> 'どのセクションを非表示にしますか?',
+	'HIDEPAGES' 					=> '<strong>ページ</strong>セクションを非表示にしますか?',
+	'HIDEFILES' 					=> '<strong>ファイル</strong>セクションを非表示にしますか?',
+	'HIDETHEMES' 					=> '<strong>テーマ</strong>セクションを非表示にしますか?',
+	'HIDEBACKUP' 					=> '<strong>バックアップ</strong>セクションを非表示にしますか?',
+	'HIDEPLUGIN' 					=> '<strong>プラグイン</strong>セクションを非表示にしますか?',
+	'HIDESUPPORT' 					=> '<strong>サポート</strong>セクションを非表示にしますか?',
+	'HIDESETTINGS' 					=> '<strong>MassiveAdmin 設定</strong>セクションを非表示にしますか?',
+	'HIDEGSSETTINGS'				=> '<strong>ページ設定</strong>] セクションを非表示にしますか?',
+	'HIDEI18NGALLERY' 				=> '<strong>i18n ギャラリー</strong>セクションを非表示にしますか? (存在する場合)',
+	'HIDEUSERS' 					=> '次のユーザーのセクションを非表示:',
+
+	//user manager
+	'USERMANAGER' 					=> 'ユーザーの管理',
+
+	//user help settings
+	'USERHELPTITLE' 				=> 'ユーザーヘルプ設定',
+	'TURNON' 						=> 'オンにしますか?',
+	'HELP' 							=> 'ヘルプ'、
+	'USERHELP' 						=> 'ユーザーヘルプ',
+
+	//menu external
+	'MENUEXTERNAL' 					=> 'カスタム メニュー リンク マネージャー',
+	'LINKNAME' 						=> 'リンク名:',
+	'LINKURL' 						=> 'リンク URL:',
+	'LINKICON' 						=> 'リンクアイコン',
+	'NEWWINDOW' 					=> 'リンクを新しいウィンドウで開きますか?',
+	'MOREICONS' 					=> '(その他のアイコンはここで見つけます。):',
+	'ADDLINK' 						=> '新しいリンクを追加',
+
+	//own footer option
+	'OWNFOOTERTITLE' 				=> 'ホワイトラベルオプション',
+	'OWNFOOTERNAME' 				=> 'カスタム フッター名:',
+	'OWNFOOTERICON' 				=> 'カスタム フッター アイコン (最大高さ 256 ピクセル | .jpg、.jpeg、.png、.gif)',
+	'OWNLOGO' 						=> 'ログインページにカスタムロゴを追加しますか?',
+	'OWNFOOTERLINK' 				=> 'カスタム フッター リンク:',
+	'OWNFOOTERHEADER' 				=> 'カスタムヘッダーCSS/スクリプト:',
+	'OWNFOOTERFOOTER' 				=> 'カスタム フッター スクリプト:',
+
+	//owncolor
+	'CHANGETITLECOLOR' 				=> 'CMS カラーの変更',
+	'MAINCOLOR' 					=> 'メインカラー:',
+	'BGCOLOR' 						=> '背景色:',
+	'CHANGENAME' 					=> 'ニックネームを管理者のニックネーム以外の名前に変更します',
+
+	//snippet
+	'SNIPPET' 						=> 'スニペット'、
+	'ADDSNIPPET' 					=> 'スニペットを追加',
+	'EDITSNIPPET' 					=> 'スニペットの編集',
+	'TITLESNIPPET' 					=> 'タイトル (スペースなし)',
+	'SUBMITSNIPPET' 				=> 'スニペットを保存',
+
+	//migrate
+	'MIGRATETITLE' 					=> 'サイト移行と SSL',
+	'MIGRATEDOMAIN' 				=> 'ドメイン移行',
+	'OLDDOMAIN' 					=> '古い Web サイトのアドレス',
+	'NEWDOMAIN' 					=> '新しい Web サイトのアドレス',
+	'REPLACEDOMAIN' 				=> 'URL を置換',
+	'FORCESSL' 						=> 'SSL を強制',
+	'TURNONSSL' 					=> 'URL で SSL を強制しますか?',
+	'SAVESSL' 						=> 'SSL設定を保存',
+
+	//plugin downloader
+	'PLUGINDOWNLOADER' 				=> 'CE プラグインをダウンロード',
+	'DOWNLOADERBASED' 				=> 'Get Simple CMS CE プラグイン リポジトリに基づいています。',
+	'SEARCHPLUGIN' 					=> 'プラグインを検索...',
+	'DOWNLOAD' 						=> 'ダウンロード'、
+
+	//login options
+	'LOGINOPTIONS' 					=> 'ログイン オプション',
+	'TURNONSHOWPASSWORDTITLE' 		=> '「パスワードを表示」ボタン',
+	'SHOWPASSWORDQUESTION' 			=> 'ログイン時に「パスワードを表示」ボタンを有効にしますか?',
+	'REMOVEFORGETPASSWORDTITLE' 	=> '「パスワードをお忘れですか?」を削除',
+	'SAVESETTINGS' 					=> '設定を保存',
+
+	//GSCONFIG
+	'GSCONFIGTITLE' 				=> 'GSCONFIG 設定の編集',
+	'GSCONFIGSAVE' 					=> 'GSCONFIG 設定を保存',
+
+	'UNISTALLER' 					=> 'プラグインをアンインストール',
+	'UNISTALLQUESTION' 				=> 'アンインストールします',
+
+	//mToper
+	"TAB_PAGES" 					=> 'ページ',
+	"TAB_FILES" 					=> 'ファイル',
+	"TAB_THEME" 					=> 'テーマ',
+	"TAB_BACKUPS" 					=> 'バックアップ',
+	"PLUGINS_NAV" 					=> 'プラグイン',
+	"TAB_SETTINGS" 					=> '設定',
+	"TAB_LOGOUT" 					=> 'ログアウト',
+
+	//makeFileInTheme
+	"CREATENEWFILE" 				=> '新しいファイルを作成',
+	"FILECREATED" 					=> 'ファイルが作成されました!',
+	
+	//frontend settings
+
+
+	//new on 4.0
+
+	//admin settings
+
+	'FRONTENDTITLE' 				=> 'フロントエンド設定',
+	'TURNONMTOPER' 					=> 'フロントエンド バーをオンにしますか?',
+	'STYLEINFO' 					=> 'フロントエンドサイドバーのスタイル',
+	'ADMINTHEMESELECTOR' 			=> '管理テーマセレクター',
+
+	//manage users
+
+	'EMAIL' 						=> '電子メール:',
+	'PASSWORD' 						=> 'パスワード:',
+	'LANG' 							=> '言語:',
+	'CHANGEPLACEHOLDER' 			=> '変更したい場合のみ変更してください',
+
+
+	//new on 5.0
+
+	'FILECREATED' 					=> 'ファイルが作成されました!',
+	'WATCHEXAMPLE' 					=> 'サンプル構造を表示',
+	'HOWUSETHEMECONFIG' 			=> 'テンプレートの設定を使用したい場合は、使用しているテンプレートのフォルダーに <code style="color:blue;">settings.json</code> を作成し、適切な構造を適用してください。例のように。次に、戻り関数の <b style="color:green;">mats("fieldname")</b> または <b style="color:green;">r_mats("fieldname")</b> を適用します。適切なフィールド。',
+	'HOWCREATETITLE' 				=> '独自の設定を作成するには?',
+	'TUTORIALHOWCREATESETTINGS' 	=> 'さまざまなタイプのオプションを備えたこの例に基づいた構造で、テーマ フォルダーのルートに <code style="color:blue;">settings.json</code> を作成します',
+	'THEMECONFIGURATORNAME' 		=> 'テーマコンフィギュレーター',
+
+	'BACKUPCREATOR' 				=> 'バックアップ作成者',
+	'CREATEBACKUP' 					=> 'バックアップの作成',
+	'SETTINGS' 						=> '設定'、
+	'NOSETTINGSCREATED' 			=> 'settings.json は作成されませんでした。',
+	'SHOWPASSWORD' 					=> 'パスワードを表示'
+
+];
